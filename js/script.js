d201dd3d2adc9f27a1ebe7214ed72e55114a6fd0
fael <fael.marques@icloud.com>
2025-03-08 21:29:59 -0300

    adicionado botão instagram

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ function mensagem(){
 function trotesolidario(){
     window.location.href = "https://www.unifacef.com.br/extensao/trote-solidario/"
 }
+function instagram(){
+
+} window.location.href = "https://www.instagram.com/rafael.fmarques_/"
