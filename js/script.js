3291d26a583181b2e9c72efb8ecbe99f64e56f49
fael <fael.marques@icloud.com>
2025-03-13 14:10:40 -0300

    redirect trote pagina

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function mensagem(){
     alert('Obrigado por me seguir!')
 }
 function trotesolidario(){
-    window.location.href = "https://www.unifacef.com.br/extensao/trote-solidario/"
+    window.location.href = "file:///C:/Users/rafae/Documents/UNI-Facef/progcomp1/trote/index.html"
 }
 function instagram(){
     window.location.href = "https://www.instagram.com/rafael.fmarques_/"
